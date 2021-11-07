ff63cafd5a5e413005e8c51df9da573e7b89ff42
Max <erosspd@gmail.com>
2021-11-07 20:30:24 +0300

    New file Scanner

---
+++ Learn_1.kt
@@ -0,0 +1,11 @@
+// Read Data Input in Kotlin example program
+// Data Input Kotlin Programs, Basic Kotlin Programs
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val scanner  = Scanner(System.`in`)
+    val str = scanner.nextLine();
+    println("I am input now: >>> $str");
+
+}
